6ee337f13cb14d19551dfc05a05718caa9bba0dc
Jenny Noonan <jennifertnoonan@gmail.com>
2018-07-12 02:21:03 +0000

    update for initial deploy

---
+++ exercise.rb
@@ -0,0 +1,16 @@
+# input should be a string
+# output should be boolean
+
+
+
+def isPalindrome(inputString)
+    return inputString.reverse == inputString
+end
+
+#input should be 2 numbers and math expression
+#output should be a number
+
+puts isPalindrome("racecar")
+
+def calculator(number1,number2,)
+    
